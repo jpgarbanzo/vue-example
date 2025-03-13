c2d0bf5dbe3a16228f4c151fd355dbb05f4792b2
Jose Pablo Garbanzo <jose.garbanzo@huli.io>
2025-03-12 19:48:42 -0600

    Adding event handler

--- to-do-list/src/components/ToDoList.vue
+++ to-do-list/src/components/ToDoList.vue
@@ -1,14 +1,18 @@
 <template>
   <h1>{{ titulo }}</h1>
   <div class="formulario">
-    <input />
-    <button>Add</button>
+    <input v-model="tareaNueva" />
+    <button v-on:click="agregarElemento">Add</button>
   </div>
+
+  {{ tareaNueva }}
 
   <h2>Task List</h2>
 
   <ul>
-    <li>{{ listaDeTareas }}</li>
+    <li v-for="tarea in listaDeTareas">
+      {{ tarea }}
+    </li>
   </ul>
 </template>
 
@@ -17,8 +21,15 @@ export default {
   data() {
     return {
       titulo: 'Aprendizaje de Vue',
-      listaDeTareas: ['Instalar Vue'],
+      listaDeTareas: ['Instalar Vue', 'Abrirlo en el navegador'],
+      tareaNueva: 'La tarea que crea la persona',
     }
+  },
+
+  methods: {
+    agregarElemento() {
+      alert('hola')
+    },
   },
 }
 </script>
