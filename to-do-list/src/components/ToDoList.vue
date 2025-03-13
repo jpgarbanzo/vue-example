413a507c4f2dd7d0783555c5788520ee29ff6a5b
Jose Pablo Garbanzo <jose.garbanzo@huli.io>
2025-03-12 18:25:00 -0600

    Initial commit

---
+++ to-do-list/src/components/ToDoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>{{ titulo }}</h1>
+  <div class="formulario">
+    <input />
+    <button>Add</button>
+  </div>
+
+  <h2>Task List</h2>
+
+  <ul>
+    <li>wash dishes</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Aprendizaje de Vue',
+      listaDeTareas: ['Instalar Vue'],
+    }
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  color: black !important;
+}
+
+.formulario {
+  display: block;
+  background-color: red;
+}
+</style>
